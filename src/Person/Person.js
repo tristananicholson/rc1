5f424f965feac2a6324ddae878bf9246aec4d31e
tnicholson1 <tnicholson1@tamucc.edu>
2020-07-24 22:18:25 -0600

    stuff done

---
+++ src/Person/Person.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Person.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+        width: 60%;
+        margin: 16px auto;
+        border: 1px solid #EEE;
+        box-shadow: 0 2px 3px #ccc;
+        padding: 16px;
+        text-align: center;
+        @media (min-width: 800px) {
+            width: 450px;
+        }        
+`;
+
+const person = (props) => {
+
+    return (
+        <StyledDiv>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name}></input>
+        </StyledDiv>
+    );
+};
+
+export default person;
